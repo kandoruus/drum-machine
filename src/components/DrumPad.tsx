baf85ec3249e54d2bc0886e9f3baa5904a853c14
caleb <1d0nt3v3nl1ftbr0@gmail.com>
2023-02-07 11:39:15 -0500

    tests all pass

--- src/components/DrumPad.tsx
+++ src/components/DrumPad.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { updateDisplay } from "../app/actionCreators";
 
@@ -12,11 +11,11 @@ type Props = {
 export const DrumPad:React.FC<Props> = ({ buttonID, audioID, audioSRC }) => {
 
   const dispatch: DispatchType = useDispatch();
-  const soundEffect = useRef(null);
+  const soundEffect: React.MutableRefObject<HTMLAudioElement | null> = useRef(null);
 
   const playSound = () => {
     if(soundEffect.current) {
-      (soundEffect.current as HTMLMediaElement).play();
+      soundEffect.current.play();
     }
   }
 
@@ -24,8 +23,26 @@ export const DrumPad:React.FC<Props> = ({ buttonID, audioID, audioSRC }) => {
     event.preventDefault();
     dispatch(updateDisplay(buttonID));
     playSound();
-
   }
+
+  const handleKeyPress = useCallback((event: KeyboardEvent) => {
+    switch(event.key){
+      case audioID:
+      case audioID.toLowerCase():
+        dispatch(updateDisplay(buttonID));
+        playSound();
+        break;
+      default:
+        break;
+    }
+  }, [audioID, buttonID, dispatch]);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyPress);
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [handleKeyPress]);
 
   return (
     <button className='drum-pad' id={ buttonID } onClick={ handleClick }>
